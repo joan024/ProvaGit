69e96f7a1f46dd08a8a8ec8a00b43f1564a6f7a1
joan024 <joan024@LAPTOP-7E3GBULT>
2022-12-19 16:25:04 +0100

    Afegeixo variable n i la mostru per pantalla

--- src/main.java
+++ src/main.java
@@ -3,6 +3,12 @@ public class main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("klk");
+		
+		int n=0;
+		n++;
+		
+		System.out.println(n);
 		
 	}
 
